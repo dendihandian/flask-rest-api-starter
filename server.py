1732f35f0ef878a42a20256a10862d10b6c32a2e
Dendi Handian <handian.dendi@gmail.com>
2018-09-24 04:16:56 +0700

    examples list and detail dummy

--- server.py
+++ server.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+examples = [
+	{
+		'id': 1,
+		'stringExample': 'lorem ipsum dolor sit amet',
+		'numberExample': 5000,
+		'booleanExample': True,
+		'floatExample': 3.14
+	},
+	{
+		'id': 2,
+		'stringExample': 'lorem ipsum dolor sit amet',
+		'numberExample': 5000,
+		'booleanExample': True,
+		'floatExample': 3.14
+	}
+]
 
 @app.route('/')
 def hello_world():
 	return 'Hello World!'
+
+@app.route('/examples')
+def exampleList():
+	return jsonify({'data': examples})
+
+@app.route('/examples/<int:exampleId>')
+def exampleDetail(exampleId):
+	return_value = {}
+	for example in examples:
+		if example['id'] == exampleId:
+			return_value = example
+			break
+
+	return jsonify(return_value)
 
 app.run(port=5000)
